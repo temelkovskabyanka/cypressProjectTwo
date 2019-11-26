36ea6250a18f4854396f0dc6cd06208106378f8b
temelkovskabyanka <temelkovska.byanka@gmail.com>
2019-11-26 20:24:17 +0000

    items changes

--- cypress/components/Component/Component.js
+++ cypress/components/Component/Component.js
@@ -1,17 +1,42 @@
 export default class Component {
-    get LogInButton(){
+    get LogInButton() {
         return cy.get("[type=\"submit\"]");
     }
-    get itemsField(){
+
+    get itemsField() {
         return cy.get(".items");
     }
-    get editButton(){
-        return cy.get(".btn.btn-more.edit_action");
+
+    get searchField() {
+        return cy.get("#search");
     }
-    get descriptionField(){
+
+    get editButton() {
+        return cy.get("#table_holder .actions .edit_action");
+    }
+
+    get category() {
+        return cy.get("#add_category");
+    }
+
+    get categoryName() {
+        return cy.get("#category_name");
+    }
+    get categoryColour(){
+        return cy.get("#category_color");
+    }
+
+    get descriptionField() {
         return cy.get("#description");
     }
-    get submitButton(){
+
+    get saveButton() {
         return cy.get("#submitf");
+    }
+    get returnToItemsButton(){
+        return cy.get("[data-bb-handler=\"confirm\"]");
+    }
+    get getTableResultsFirstRow(){
+        return cy.get("#sortable_table tbody tr").eq(0);
     }
 }
